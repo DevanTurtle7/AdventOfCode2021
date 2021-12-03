0fa116692c300bdf74ad5e32021c5e1bf45cde32
Devan Kavalchek <devankav7@gmail.com>
2021-12-03 11:53:28 -0500

    refactored

--- Day02/Solution.kt
+++ Day02/Solution.kt
@@ -13,9 +13,26 @@ class Position(var horiz: Int, var depth: Int) {
             "forward" -> horiz += instruction.amount
         }
     }
+
+    fun finalPosition() = horiz * depth
 }
 
-fun parseData(filename: String): Position {
+class Sub(var horiz: Int, var depth: Int, var aim: Int) {
+    infix fun move(instruction: Instruction) {
+        when(instruction.command) {
+            "down" -> aim += instruction.amount
+            "up" -> aim -= instruction.amount
+            "forward" -> {
+                horiz += instruction.amount
+                depth += aim * instruction.amount
+            }
+        }
+    }
+
+    fun finalPosition() = horiz * depth
+}
+
+fun parseDataPart1(filename: String): Position {
     val inputStream: InputStream = File(filename).inputStream()
     val position = Position(0, 0)
 
@@ -32,10 +49,28 @@ fun parseData(filename: String): Position {
     return position
 }
 
+fun parseDataPart2(filename: String): Sub {
+    val inputStream: InputStream = File(filename).inputStream()
+    val sub = Sub(0, 0, 0)
+
+    inputStream.bufferedReader().forEachLine {
+        val trimmed = it.trim()
+        val tokens: List<String> = trimmed.split(" ")
+        val command = tokens[0]
+        val amount = tokens[1].toInt()
+
+        val instruction = Instruction(command, amount)
+        sub move instruction
+    }
+
+    return sub
+}
+
 fun main() {
-    val finalPosition = parseData("./input.txt")
+    val finalPosition = parseDataPart1("./input.txt")
     val horiz = finalPosition.horiz
     val depth = finalPosition.depth
+    val result = finalPosition.finalPosition()
 
-    println("Horiz: $horiz, Depth: $depth, Mult: ${horiz * depth}")
+    println("Horiz: $horiz, Depth: $depth, Mult: ${result}")
 }
